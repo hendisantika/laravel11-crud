79583eff30951011aa91ade9b4a5b8955dec8be0
hendisantika <hendisantika@yahoo.co.id>
2024-05-30 06:12:31 +0700

    Modify ProductStoreRequest🫸🌀✏️📗🐧🐳

--- app/Http/Requests/ProductStoreRequest.php
+++ app/Http/Requests/ProductStoreRequest.php
@@ -23,7 +23,8 @@ class ProductStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required',
+            'detail' => 'required'
         ];
     }
 }
